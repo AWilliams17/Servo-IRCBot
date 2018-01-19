07ae20dad660323b33d7b50a05ce519b28e82599
awilliams17 <awilliams17411@gmail.com>
2018-01-18 22:13:09 -0600

    ircformatting.py now uses enums instead of whatever the hell i was using before lol

--- servomodules/ircformatting.py
+++ servomodules/ircformatting.py
@@ -1,43 +1,45 @@
 # ToDo: Allow for the mixing of formats and colors(EG: A bold and red string).
 # ToDo: Fix broken multi-line formatting/coloring
-
-color_codes = {
-    "white": "00", "black": "01", "blue": "11",
-    "green": "03", "red": "04", "brown": "05",
-    "purple": "06", "orange": "07", "yellow": "08",
-    "lightgreen": "09", "teal": "10", "grey": "12",
-    "pink": "13"
-}
-
-style_codes = {
-    "bold": "\x02", "italic": "\x1D",
-    "underlined": "\x1F"
-}
+from enum import Enum
 
 
-def change_style(string, style):
+class ColorCodes(Enum):
+    WHITE = "00"
+    BLACK = "01"
+    BLUE = "11"
+    GREEN = "03"
+    RED = "04"
+    BROWN = "05"
+    PURPLE = "06"
+    ORANGE = "07"
+    YELLOW = "08"
+    LIGHT_GREEN = "09"
+    TEAL = "10"
+    GREY = "12"
+    PINK = "13"
+
+
+class StyleCodes(Enum):
+    BOLD = "\x02"
+    ITALIC = "\x1D"
+    UNDERLINED = "\x1F"
+
+
+def change_style(string, style_type):
     """
     Changes the style of an IRC string.
     :param string: The string to apply styling to.
-    :param style: The style you want.
+    :param style_type: The value of the style you want. grab from StyleCodes.(style)
     :return: Returns the styled string.
     """
-    if style in style_codes:
-        desired_style = style_codes.get(style)
-        return "%s%s%s" % (desired_style, string, desired_style)
-    else:
-        raise ValueError("The style '%s' does not exist! Valid styles: %s" % (str(style), style_codes.keys()))
+    return "%s%s%s" % (style_type.value, string, style_type.value)
 
 
 def change_color(string, color):
     """
     Changes the color of an IRC string.
     :param string: The string to color.
-    :param color: The color you want.
+    :param color: The value of the color you want. grab from ColorCodes.(color)
     :return: Returns the colored string.
     """
-    if color in color_codes:
-        desired_color = color_codes.get(color)
-        return "%s%s%s%s" % ("\x03", desired_color, string, "\x03")
-    else:
-        raise ValueError("The color '%s' does not exist! Valid colors: %s" % (str(color), color_codes.keys()))
+    return "%s%s%s%s" % ("\x03", color.value, string, "\x03")
